ffbd114339f5aaa9116cfde08ad87b16978b37c3
KowalewskiPawel <pawelkowalewsk@gmail.com>
2025-03-17 21:03:58 +0100

    Initial Commit

---
+++ iTARS/iTARSApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTARSApp.swift
+//  iTARS
+//
+//  Created by Pawel Kowalewski on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct iTARSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
